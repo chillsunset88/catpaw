7bb65751bd1424f19fde5ea69fb5447775a076f8
Xins22 <hanif.davi@gmail.com>
2025-10-06 10:38:49 +0700

    1. hanif Drawer

--- app/src/main/java/com/example/catpaw/MainActivity.kt
+++ app/src/main/java/com/example/catpaw/MainActivity.kt
@@ -1,43 +1,79 @@
 package com.example.catpaw
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.viewpager2.widget.ViewPager2
-import com.example.catpaw.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Tampilkan fragment default pertama kali
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        navView.setNavigationItemSelectedListener(this)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, FeedFragment())
-                .commit()
+            replaceFragment(FeedFragment())
+            navView.setCheckedItem(R.id.nav_home)
         }
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNav.setOnItemSelectedListener { item ->
-            val selectedFragment = when (item.itemId) {
+            val selectedFragment: Fragment? = when (item.itemId) {
                 R.id.nav_feed -> FeedFragment()
                 R.id.nav_find -> FindFragment()
                 R.id.nav_profile -> ProfileFragment()
-                else -> FeedFragment()
+                else -> null
             }
-
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, selectedFragment)
-                .commit()
+            if (selectedFragment != null) {
+                replaceFragment(selectedFragment)
+            }
             true
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val selectedFragment: Fragment = when (item.itemId) {
+            R.id.nav_home -> FeedFragment()
+            R.id.nav_gallery -> GalleryFragment()
+            R.id.nav_slideshow -> SlideshowFragment()
+            else -> FeedFragment()
+        }
+        replaceFragment(selectedFragment)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun replaceFragment(Fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, Fragment)
+            .commit()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 }
